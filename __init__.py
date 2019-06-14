63d834e5d74c18925fbb4b3814bdf2139cc813a8
Michal Frystacky <Michal.Frystacky@gmail.com>
2019-06-13 21:13:28 -0700

    Feat: Notification when extension is (un)loaded

--- __init__.py
+++ __init__.py
@@ -9,11 +9,13 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
+    ctx.send(f'**cog {extension} is loaded.**')
 
 
 @bot.command()
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
+    ctx.send(f'**cog {extension} is unloaded.**')
 
 
 for filename in os.listdir('./cogs'):
