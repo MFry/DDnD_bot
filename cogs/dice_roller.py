9a432121a1ce7ed6c84ebf952dc0afcc8c3b4790
unknown <sffreemans@gmail.com>
2019-06-16 14:27:56 -0600

    Fix: Handling for omitted dice number, formatting

--- cogs/dice_roller.py
+++ cogs/dice_roller.py
@@ -9,20 +9,23 @@ class DiceRoller(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Dice rolling cog is loaded')
+        print('Dice Roller *cog* is loaded')
 
     @commands.command()
     async def roll(self, ctx, dice: str):
         cleaned_dice = dice.lower()
-        dice_list = dice.split('d')
-        dice_number = int(dice_list[0])
+        dice_list = cleaned_dice.split('d')
+        if not dice_list[0]:
+            dice_number = 1
+        else:
+            dice_number = int(dice_list[0])
         dice_type = int(dice_list[1])
         if dice_number == 1:
             dice_total = random.randint(1, dice_type)
             await ctx.send(
                 f'*{ctx.message.author} rolls {dice} for {dice_total}*'
             )
-        elif dice_number > 1
+        elif dice_number > 1:
             result = []
             dice_total = 0
             ii = 0
@@ -30,7 +33,6 @@ class DiceRoller(commands.Cog):
                 result.append(random.randint(1, dice_type))
                 dice_total += result[ii]
                 ii += 1
-
             rolls = ", ".join(map(str, result))
             await ctx.send(
                 f'*{ctx.message.author} rolls {dice} for: {rolls}; a total of {dice_total}*'
