51881bb876e048a9974b7eccebd3cfd9e207d5ea
unknown <sffreemans@gmail.com>
2019-06-22 21:30:52 -0600

    Fix: added comments, help documentation

--- cogs/dice_roller.py
+++ cogs/dice_roller.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import random
 
+#Accepts an arbitrary number and type of dice as input of the form xdy, where 'x'
+#is the integer number of dice and 'y' is the integer number of faces on each die.
+#d is a delimiter.
 
 class DiceRoller(commands.Cog):
     def __init__(self, client):
@@ -13,19 +16,23 @@ class DiceRoller(commands.Cog):
 
     @commands.command()
     async def roll(self, ctx, dice: str):
+        """
+        Rolls an arbitrary number of dice with an arbitrary number of sides
+        :param dice: String of the form xdy, where x is the integer number of dice, and y is the integer number of sides per dice.
+        """
         cleaned_dice = dice.lower()
         dice_list = cleaned_dice.split('d')
-        if not dice_list[0]:
+        if not dice_list[0]:                                                    #handle inputs of the type 'dy', with implied dice number equal to 1
             dice_number = 1
         else:
             dice_number = int(dice_list[0])
         dice_type = int(dice_list[1])
-        if dice_number == 1:
+        if dice_number == 1:                                                    #generate a single dice roll and send
             dice_total = random.randint(1, dice_type)
             await ctx.send(
                 f'*{ctx.message.author} rolls {dice} for {dice_total}*'
             )
-        elif dice_number > 1:
+        elif dice_number > 1:                                                   #generate x number of dice rolls, send the individual rolls and sum
             result = []
             dice_total = 0
             ii = 0
@@ -37,7 +44,7 @@ class DiceRoller(commands.Cog):
             await ctx.send(
                 f'*{ctx.message.author} rolls {dice} for: {rolls}; a total of {dice_total}*'
             )
-        else:
+        else:                                                                   #handle invalid inputs
             await ctx.send(
                 f'*{ctx.message.author} rolls a non-physical number of dice and takes 5 damage*'
             )
